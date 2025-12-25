d3b2cfa2ee2c9cebc6ab239ffc4be9dbce24571b
PerryXu1 <perryxu6626@gmail.com>
2025-12-25 00:03:12 -0500

    feat: add documentation

--- src/lumen/circuit/component.py
+++ src/lumen/circuit/component.py
@@ -7,21 +7,48 @@ from uuid import uuid4
 T = TypeVar("T", bound="Component")
 
 class Component(ABC):
+    """Class representing an abstract representation of a component within the photonics circuit.
+    
+    :param name: The name of the component
+    :type name: str
+    :param num_inputs: The amount of inputs that the component has
+    :type num_inputs: int
+    :param num_outputs: The amount of outputs that the component has
+    :type num_outputs: int
+    :param s_matrix: The matrix used to model the component mathematically
+    :type s_matrix: numpy ndarray
+    """
+    
     __slots__ = "name", "s_matrix", "inputs", "outputs"
 
     def __init__(self, name: str, num_inputs: int, num_outputs: int, s_matrix: np.ndarray):
+        """Constructor method
+        """
+        
         self.id = uuid4()
         self.name = name
-
+        
         # Modified S Matrix - 2N x 2N, N = num_inputs + num_outputs = total number of ports
         # Similar to an S matrix, where the ijth componenet is the ratio of the complex amplitude
         # between the output at the ith port and the input at the jth port
         # To account for polarization, element modified to have 2 elements: one for horizontal and one for vertical
         self.s_matrix = s_matrix
+    
+        # list of inputs/outputs
         self.inputs = [Port(self) for _ in range(num_inputs)]
         self.outputs = [Port(self) for _ in range(num_outputs)]
 
     def search_by_input_alias(self, alias: str) -> Port:
+        """Returns the input port referred to by a previously-set alias for the port. If the alias
+        does not exist, an exception is thrown.
+        
+        :param alias: The alias of an input port; used to search for an input port with the
+            corresponding name
+        :type alias: str
+        :return: The input port referred to by the alias
+        :rtype: Port
+        """
+        
         from .exceptions import MissingAliasException
         for input_port in self.inputs:
             if input_port.alias == alias:
@@ -29,27 +56,64 @@ class Component(ABC):
         raise MissingAliasException(alias)
 
     def search_by_output_alias(self, alias: str) -> Port:
+        """Returns the output port referred to by a previously-set alias for the port. If the alias
+        does not exist, an exception is thrown.
+        
+        :param alias: The alias of an output port; used to search for an output port with the
+            corresponding name
+        :type alias: str
+        :return: The output port referred to by the alias
+        :rtype: Port
+        """
+        
         from .exceptions import MissingAliasException
         for output_port in self.outputs:
             if output_port.alias == alias:
                 return output_port
         raise MissingAliasException(alias)
     
-    def change_input_alias(self, index: int, name: str) -> None:
+    def set_input_alias(self, index: int, alias: str) -> None:
+        """Sets the alias of the specified input port to the specified name
+        
+        :param index: The index of the input port which will have their alias set
+        :type index: int
+        :param alias: The new alias of the input port 
+        :type alias: str
+        """
+        
         from .exceptions import DuplicateAliasException
         for input_port in self.inputs:
-            if name in input_port.alias:
-                raise DuplicateAliasException(name)
-        self.inputs[index].alias = name
+            if alias in input_port.alias:
+                raise DuplicateAliasException(alias)
+        self.inputs[index].alias = alias
 
-    def change_output_alias(self, index: int, name: str) -> None:
+    def set_output_alias(self, index: int, alias: str) -> None:
+        """Sets the alias of the specified output port to the specified name
+        
+        :param index: The index of the output port which will have their alias set
+        :type index: int
+        :param alias: The new alias of the output port 
+        :type alias: str
+        """
         from .exceptions import DuplicateAliasException
         for output_port in self.outputs:
-            if name in output_port.alias:
-                raise DuplicateAliasException(name)
-        self.outputs[index].alias = name
+            if alias in output_port.alias:
+                raise DuplicateAliasException(alias)
+        self.outputs[index].alias = alias
 
     def set_input(self, input_port_name: int | str, component: T, output_port_name: int | str) -> None:
+        """Connects a component's input port with another component's output port
+        
+        :param input_port_name: The index or alias of the output port
+        :type input_port_name: int, str
+        :param component: The other component to which this component will be attached to
+        :type component: Component
+        :param input_port_name: The index or alias of the input port
+        :type input_port_name: int, str  
+        """
+        
+        # int passed in -> identify port by index
+        # str passed in -> identify port by alias
         if isinstance(output_port_name, int):
             output_port = component.outputs[output_port_name]
         elif isinstance(output_port_name, str):
@@ -63,6 +127,18 @@ class Component(ABC):
         input_port.connected_port = output_port
     
     def set_output(self, output_port_name: int | str, component: T, input_port_name: int | str) -> None:
+        """Connects a component's output port with another component's input port
+        
+        :param input_port_name: The index or alias of the input port
+        :type input_port_name: int, str
+        :param component: The other component to which this component will be attached to
+        :type component: Component
+        :param input_port_name: The index or alias of the output port
+        :type input_port_name: int, str  
+        """
+        
+         # int passed in -> identify port by index
+        # str passed in -> identify port by alias
         if isinstance(output_port_name, int):
             output_port = self.outputs[output_port_name]
         elif isinstance(output_port_name, str):
