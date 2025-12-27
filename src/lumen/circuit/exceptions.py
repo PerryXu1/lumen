0dca2cfc06b1298f4d1de0f85eb49f651a0a50b3
PerryXu1 <perryxu6626@gmail.com>
2025-12-27 15:07:54 -0500

    feat: add exception for missing ports

--- src/lumen/circuit/exceptions.py
+++ src/lumen/circuit/exceptions.py
@@ -1,5 +1,8 @@
 from typing import Optional
-from .component import Component
+
+from circuit.component import Component
+
+from ..models.port import Port
 
 class DuplicateAliasException(Exception):
     """Exception thrown when two inputs or two outputs have the same alias.
@@ -14,9 +17,6 @@ class DuplicateAliasException(Exception):
     __slots__ = "alias", "message"
     
     def __init__(self, alias: str, message: Optional[str]):
-        """Constructor method
-        """
-        
         self.message = message
         self.alias = alias
         super().__init__(self.alias, self.message)
@@ -43,9 +43,6 @@ class MissingAliasException(Exception):
     __slots__ = "alias", "message"
     
     def __init__(self, alias: str, message: Optional[str]):
-        """Constructor method
-        """
-        
         self.message = message
         self.alias = alias
         super().__init__(self.alias, self.message)
@@ -62,6 +59,34 @@ class MissingAliasException(Exception):
             return f"'{self.alias}' does not exist"
         return self.message
 
+class MissingPortException(Exception):
+    """Exception thrown when the port referred to in a circuit does not exist.
+    
+    :param port: The alias used for search that does not exist
+    :type port: Port
+    :param message: A message printed when the exception is thrown. If no message
+        is given, a default message is printed
+    :type message: optional str
+    """
+    __slots__ = "port", "message"
+    
+    def __init__(self, port: Port, message: Optional[str]):
+        self.port = port
+        self.message = message
+        super().__init__(self.port, self.message)
+        
+    def __str__(self):
+        """Method that defines the message printed when the exception is thrown.
+        Either a custom message passed into the constructor or the default message.
+        
+        :return: A message to be printed when the exception is thrown
+        :rtype: str
+        """
+        
+        if self.message is None:
+            return f"{self.port.id} not found in the circuit"
+        return self.message
+
 class MissingComponentException(Exception):
     """Exception thrown when the component referred to in a circuit does not exist.
     
@@ -71,12 +96,10 @@ class MissingComponentException(Exception):
         is given, a default message is printed
     :type message: optional str
     """
+    
     __slots__ = "component", "message"
     
-    def __init__(self, component: Component, message: Optional[str]):
-        """Constructor method
-        """
-        
+    def __init__(self, component: Component, message: Optional[str]):        
         self.component = component
         self.message = message
         super().__init__(self.component, self.message)
