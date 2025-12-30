22a0f1766a419dcf5c6dcf99bb7eec25f8117baa
PerryXu1 <perryxu6626@gmail.com>
2025-12-30 13:57:48 -0500

    feat: update __all__

--- src/lumen/circuit/__init__.py
+++ src/lumen/circuit/__init__.py
@@ -1,7 +1,7 @@
-from .component import Component
+from .component import Component, PortRef
 from .exceptions import DuplicateAliasException, MissingAliasException, MissingPortException
 from .photonic_circuit import PhotonicCircuit
 
 __all__ = ['Component', 'DuplicateAliasException',
            'MissingAliasException', 'MissingPortException',
-           'MissingComponentException', 'PhotonicCircuit']
+           'MissingComponentException', 'PhotonicCircuit', 'PortRef']
