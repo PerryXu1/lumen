48a2631a3b849b17aa31812dcd758e758ff5ed1f
PerryXu1 <perryxu6626@gmail.com>
2025-12-30 13:59:41 -0500

    feat: use port references in photonic circuit

--- src/lumen/circuit/photonic_circuit.py
+++ src/lumen/circuit/photonic_circuit.py
@@ -2,160 +2,181 @@ from collections.abc import MutableMapping, MutableSequence, Sequence
 from typing import Optional
 from ..circuit.laser import Laser
 from ..models.port import InputConnection, OutputConnection, Port, PortConnection
-from .component import Component
-from .exceptions import MissingComponentException, MissingPortException
+from .component import Component, PortRef
+from .exceptions import MissingComponentException
 from uuid import uuid4
+
 
 class PhotonicCircuit:
     """Class representing a photonic circuit composed of components connected to one another. 
     The circuit will initially be empty and users can add components and connect them to build a
     functional circuit.
     """
-    
+
     __slots__ = "id", "_components", "circuit_inputs", "circuit_outputs"
 
     def __init__(self):
         self.id = uuid4()
-        self._components: Optional[Component] = [] # list of components in the circuit
-        self.circuit_inputs: MutableMapping[Port, Laser] = {} # the ports that the laser light inputs to
-        self.circuit_outputs: MutableSequence[Port] = [] # the ports at which the final state is desired
+        # list of components in the circuit
+        self._components: Optional[Component] = []
+        # the ports that the laser light inputs to
+        self.circuit_inputs: MutableMapping[Port, Laser] = {}
+        # the ports at which the final state is desired
+        self.circuit_outputs: MutableSequence[Port] = []
 
-        # TODO: make it so it takes in Component + index/alias, not port directly
-    def set_circuit_input(self, port: Port, laser: Laser) -> None:
+    def set_circuit_input(self, laser: Laser, port_ref: PortRef) -> None:
         """Sets a port that laser light source inputs to.
-        
-        :param port: The port that the laser light inputs to
-        :type port: Port
+
         :param laser: The laser used as input at that port
         :type laser: Laser
+        :param port_ref: The component and input port that the laser light inputs to
+        :type port_ref: PortRef
         """
-        port_found = False
-        for component in self._components:
-            if port in component._input_ports:
-                port_found = True
-                self.circuit_inputs[port] = laser
-                port.connection = InputConnection()
-        if port_found == False:
-            raise MissingPortException(port)
-    
-    # TODO: make it so it takes in Component + index/alias, not port directly
-    def set_circuit_output(self, port: Port) -> None:
+        component, port_name = port_ref
+
+        port = self._get_input_port_from_ref(
+            port_ref=PortRef(component, port_name))
+        self.circuit_inputs[port] = laser
+
+        if port.connection is None:
+            port.component._in_degree += 1
+        port.connection = InputConnection()
+
+    def set_circuit_output(self, port_ref: PortRef) -> None:
         """Sets a port as an output, where the final state is desired.
-        
-        :param port: The port that the laser light inputs to
-        :type port: Port
+
+        :param port_ref: The component and ref port that the circuit outputs to
+        :type port_ref: PortRef
         """
-        
-        port_found = False
-        for component in self._components:
-            if port in component._output_ports:
-                port_found = True
-                self.circuit_outputs.append(port)
-                port.connection = OutputConnection()
-        if port_found == False:
-            raise MissingPortException(component)
+        component, port_name = port_ref
+
+        port = self._get_output_port_from_ref(
+            port_ref=PortRef(component, port_name))
+        self.circuit_outputs.append(port)
+
+        if port.connection is None:
+            port.component._out_degree += 1
+        port.connection = OutputConnection()
 
     def add(self, component: Component) -> None:
         """Adds a component to the circuit.
-        
+
         :param component: The component to be added to the circuit
         :type component: Component
         """
-        
+
         self._components.append(component)
 
-    def connect(self, component1: Component, output_name: str | int, component2: Component, input_name: str | int) -> None:
+    def connect(self, source: PortRef, destination: PortRef) -> None:
         """Connect the output of one component in the circuit to the input of another component
         in the circuit.
-        
-        :param component1: The component whose output is connected
-        :type component1: Component
-        :param output_name: The alias/index of the output port of component1 that is connected. 
-        :type output_name: str (for aliases), int (for indices)
-        :param component2: The component whose input is connected
-        :type component2: Component
-        :param input_name: The alias/index of the input port of component2 that is connected. 
-        :type input_name: str (for aliases), int (for indices)
+
+        :param source: The component and output port name that is connected.
+        :type source: PortRef
+        :param destination: The component and input port name that is connected to.
+        :type destination: PortRef
         """
+
+        component1, output_name = source
+        component2, input_name = destination
+        print(self._components)
         
+        print(component1, component2)
+
         if component1 not in self._components or component2 not in self._components:
-            raise MissingComponentException("One or both of the components are not in the circuit")
+            raise MissingComponentException(
+                "One or both of the components are not in the circuit")
         
-        if isinstance(output_name, str):
-            output_port = component1.search_by_output_alias(output_name)
-        elif isinstance(output_name, int):
-            output_port = component1._output_ports[output_name]
-        if isinstance(input_name, str):
-            input_port = component2.search_by_input_alias(input_name)
-        elif isinstance(input_name, int):
-            input_port = component2._input_ports[input_name]
-            
+        input_port = self._get_input_port_from_ref(source)
+        output_port = self._get_output_port_from_ref(destination)
+
         if output_port in self.circuit_outputs:
             self.circuit_outputs.remove(output_port)
         if input_port in self.circuit_inputs:
             self.circuit_inputs.pop(input_port)
-            
-        component1.connect_output_port(output_name, component2, input_name)
-        component2.connect_input_port(input_name, component1, output_name)
-    
+
+        component1.connect_output_port(
+            output_name, to=PortRef(component2, input_name))
+        component2.connect_input_port(
+            input_name, to=PortRef(component1, output_name))
+
     def _connect_by_port(self, port1: Port, port2: Port) -> None:
         port1.connection = PortConnection(port2)
         port2.connection = PortConnection(port1)
 
-    def disconnect_by_input(self, component: Component, input_port_name: str | int) -> None:
+    def disconnect_by_input(self, *, port_ref: PortRef) -> None:
         """Disconnects a component's input from another component's output and vice versa.
-        
-        :param component: The component whose input is to be disconntected
-        :type component: Component
-        :param input_port_name: The alias/index of the input port of component1 that is 
-            to be disconnected. 
-        :type input_port_name: str (for aliases), int (for indices)
+
+        :param port_ref: The component and input port to be disconntected
+        :type port_ref: PortRef
         """
-        
+        component, input_port_name = port_ref
+
         if component not in self._components:
             raise MissingComponentException(component)
-        
-        if isinstance(input_port_name, str):
-            input_port = component.search_by_input_alias(input_port_name)
-        elif isinstance(input_port_name, int):
-            input_port = component._input_ports[input_port_name]
-        
+
+        input_port = self._get_input_port_from_ref(port_ref)
         output_port = input_port.connection
+        
         if isinstance(output_port, PortConnection):
             other_component = output_port.port.component
             other_component.disconnect_output()
         component.disconnect_input(input_port_name)
 
-    def disconnect_by_output(self, component: Component, output_port_name: str | int) -> None:
+    def disconnect_by_output(self, *, port_ref: PortRef) -> None:
         """Disconnects a component's input from another component's output and vice versa.
-        
-        :param component: The component whose input is to be disconntected
-        :type component: Component
-        :param input_port_name: The alias/index of the input port of component1 that is 
-            to be disconnected. 
-        :type input_port_name: str (for aliases), int (for indices)
+
+        :param port_ref: The component and output port to be disconntected
+        :type port_ref: PortRef
         """
-        
+        component, output_port_name = port_ref
+
         if component not in self._components:
             raise MissingComponentException(component)
-        
-        if isinstance(output_port_name, str):
-            output_port = component.search_by_input_alias(output_port_name)
-        elif isinstance(output_port_name, int):
-            output_port = component._input_ports[output_port_name]
-        
+
+        output_port = self._get_output_port_from_ref(port_ref=port_ref)
         input_port = output_port.connection
+
         if isinstance(input_port, PortConnection):
             other_component = input_port.port.component
             other_component.disconnect_input()
+
         component.disconnect_output(output_port_name)
 
     @property
     def components(self) -> Sequence[Component]:
         """The list of components contained within the circuit. Property of the class.
-        
+
         :return: The list of components contained within the circuit
         :rtype: list of Component
         """
-        
+
         return self._components
+
+    def _get_input_port_from_ref(self, port_ref: PortRef) -> Port:
+        from .exceptions import MissingAliasException
+
+        component, port_name = port_ref
+
+        if isinstance(port_name, int):
+            port = component._input_ports[port_name]
+        elif isinstance(port_name, str):
+            if port_name in component._input_port_aliases:
+                port = component._input_port_aliases[port_name]
+            else:
+                raise MissingAliasException(port_name)
+        return port
+
+    def _get_output_port_from_ref(self, port_ref: PortRef) -> Port:
+        from .exceptions import MissingAliasException
+
+        component, port_name = port_ref
+
+        if isinstance(port_name, int):
+            port = component._output_ports[port_name]
+        elif isinstance(port_name, str):
+            if port_name in component._output_port_aliases:
+                port = component._output_port_aliases[port_name]
+            else:
+                raise MissingAliasException(port_name)
+        return port
