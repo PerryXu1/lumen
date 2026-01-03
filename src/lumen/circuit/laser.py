b3246e0b8ac34f1376a8ee1301a62f0f2aca8fcb
PerryXu1 <perryxu6626@gmail.com>
2026-01-03 11:46:50 -0500

    feat: modify laser to make laser wavelength constant

--- src/lumen/circuit/laser.py
+++ src/lumen/circuit/laser.py
@@ -1,25 +1,28 @@
 from collections.abc import Callable
-from ..models.light import Light
+from ..models.light import CoherentLight, FixedWavelengthCoherentLight
 
 class Laser:
     """Representation of a laser input into the circuit
     
     :param light_func: a function mapping a time to a light state
-    :type light_func: Callable[float] -> Light
+    :type light_func: Callable[float] -> CoherentLight
     """
+
     # add more parameters
-    __slots__ = "_light_func",
+    __slots__ = "_light_func", "wavelength"
     
-    def __init__(self, *, light_func: Callable[[float], Light]):
+    def __init__(self, *, light_func: Callable[[float], FixedWavelengthCoherentLight], wavelength: float):
         self._light_func = light_func
+        self.wavelength = wavelength
     
-    def __call__(self, t: float) -> Light:
+    def __call__(self, t: float) -> CoherentLight:
         """Makes the class's light function callable directly through the class
         
         :param t: time
         :type t: float
         :return: The value of the light function at the specified time
-        :rtype: Light
+        :rtype: CoherentLight
         """
-        
-        return self._light_func(t)
+        eh, ev = self._light_func(t).e
+        light = CoherentLight(eh, ev, self.wavelength)
+        return light
