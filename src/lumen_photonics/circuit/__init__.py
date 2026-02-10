7193eec2dab3d8f62717712ada2b643149f75f33
PerryXu1 <perryxu6626@gmail.com>
2026-02-09 21:55:42 -0500

    feat: bug fix

--- src/lumen_photonics/circuit/__init__.py
+++ src/lumen_photonics/circuit/__init__.py
@@ -1,5 +1,5 @@
 from .component import Component, PortRef
-from .circuit_exceptions import DuplicateComponentException, DuplicateAliasException, MissingAliasException, MissingPortException
+from .circuit_exceptions import DuplicateComponentException, DuplicateAliasException, MissingAliasException, MissingPortException, MissingComponentException
 from .photonic_circuit import PhotonicCircuit
 
 __all__ = ['Component', 'DuplicateComponentException', 'DuplicateAliasException', 'MissingAliasException',
